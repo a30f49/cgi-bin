3f1e44cd2fc0e5db124cdee9f9e80272cb354ad6
vincent <vincent@example.com>
2015-10-15 23:34:56 +0800

    add INSTALL

--- lib/Plugin/ActivityGenerator.pm
+++ lib/Plugin/ActivityGenerator.pm
@@ -1,4 +1,12 @@
 package ActivityGenerator;
+=head1 SYNOPSIS
+    my $act = new Activity("app");
+    $act->target_package("com.jfeat.apps.quandian.units");
+
+    #$act->gen_with_fragment("com.jfeat.apps.quandian.module.admin.app.AdminUsersFragment");
+    $act->gen_test_with_fragment("com.jfeat.apps.quandian.module.admin.app.AdminUsersFragment");
+=cut
+
 use lib qw(lib);
 use File::chdir;
 use Cwd;
